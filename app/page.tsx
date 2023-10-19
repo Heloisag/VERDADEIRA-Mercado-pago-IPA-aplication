036f6ef627489c3afe33109c887f9485950d099e
HeloWorld <Heloisagoulartv@gmail.com>
2023-10-19 15:46:42 -0300

    feature/hgv/19102023 project config

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image'
+import Home1 from "pages/HomeScreen"
+
+export default function Home() {
+  return (
+  <Home1></Home1>
+  )
+}
---
+++ src/pages/HomeScreen/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+
+export default function Home (){
+  return (
+    <div>
+      <h1 style={{color: "#white"}}>API Mercado Pago!</h1>
+    </div>
+  )
+}
+
